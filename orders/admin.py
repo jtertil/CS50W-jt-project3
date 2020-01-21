648da3c1fa7fe399f07ce87dcd0ab105e361f1fd
jtertil <jtertil@gmail.com>
2020-01-21 11:29:57 +0100

    meal admin panels update

--- orders/admin.py
+++ orders/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 
 from .models import MealSize, MealType, Meal, Ingredient
 
-admin.site.register(Meal)
+
+class MealAdmin(admin.ModelAdmin):
+    filter_horizontal = ('available_ingredients',)
+
+
+admin.site.register(Meal, MealAdmin)
 admin.site.register(Ingredient)
 admin.site.register(MealSize)
 admin.site.register(MealType)
